f9e4120b3a0f11a48c8eb40b11c170045a9889b2
Rishabh Anand <rishabhanandxz@gmail.com>
2021-10-12 20:51:38 +0530

    Backup

---
+++ Semester5/Embedded_System/Practical/EXP_4_4525.X/build/default/debug/main.o.d
@@ -0,0 +1,3 @@
+build/default/debug/main.o:  \
+	/home/fenris/work/dead/University/Semester5/Embedded_System/Practical/EXP_4_4525.X/main.asm  \
+
---
+++ Semester5/Embedded_System/Practical/EXP_4_4525.X/main.asm
@@ -0,0 +1,9 @@
+#include p18f4520.inc
+    ORG 00
+    MOVLW 99H
+    ADDLW 01H
+    ADDLW 66H
+    ADDLW 89H
+    ADDLW 97H
+    ADDLW 35H 
+    END
