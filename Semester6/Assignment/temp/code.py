083c909e6f4271cb7c674bd99977ec9c23c133bb
Fenris Lycaon <thefenrislycaon@gmail.com>
2022-03-05 22:47:31 +0530

    Mod template

--- Semester6/Assignment/temp/code.py
+++ Semester6/Assignment/temp/code.py
@@ -1,11 +1,1 @@
-import pandas as pd
-import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
-import missingno
-
-df = pd.read_csv("train.csv")
-df.info()
-df.head()
-w
+print("Hello World!")
